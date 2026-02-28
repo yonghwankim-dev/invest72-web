0992941b5845c399cf9f09f24e816aeed4db9c75
yonghwankim-dev <dragonbead95@naver.com>
2026-02-28 15:12:07 +0900

    navBar css

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import "./NavBar.css";
 
 export default function NavBar({user, isLoggedIn, handleLogout}){
     return (
@@ -6,18 +7,26 @@ export default function NavBar({user, isLoggedIn, handleLogout}){
       <div className="logo">
         <Link to="/" className="navLink">Invest72</Link>
       </div>
+
+      <div className="navLinksContainer">
+        <ul className="navLinks">
+          {isLoggedIn && (
+            <li><Link to="/products" className="navLink">상품</Link></li>
+          )}
+        </ul>
+      </div>
+
       <ul className="navLinks">
         <li><Link to="/" className="navLink">홈</Link></li>
-                {isLoggedIn ? (
-                    <>
-                        <li><Link to="/products" className="navLink">상품</Link></li>
-                        <li><strong className="userName">{user?.nickname}</strong></li>
-                        <li><button className="buttonDanger" onClick={handleLogout}>로그아웃</button></li>
-                    </>
-                ) : (
+        {isLoggedIn ? (
+          <>
+            <li><strong className="userName">{user?.nickname}</strong></li>
+            <li><button className="buttonDanger" onClick={handleLogout}>로그아웃</button></li>
+          </>
+        ) : (
           <li><Link to="/login" className="navLink">로그인</Link></li>
-                )}
-            </ul>
-        </nav>
-    );
+        )}
+      </ul>
+    </nav>
+  );
 }
