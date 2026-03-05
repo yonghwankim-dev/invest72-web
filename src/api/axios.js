6c1711b4a2e71aa717362e0273879fc08e45e089
yonghwankim-dev <dragonbead95@naver.com>
2026-03-05 12:15:16 +0900

    add env file

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
+const HOST = process.env.REACT_APP_API_BASE_URL || "http://localhost:8080";
 const api = axios.create({
-        baseURL: "http://localhost:8080",
+        baseURL: HOST,
         withCredentials: true // 모든 세션 요청에 쿠키 포함
     });
 
--- src/components/LoginButtons.jsx
+++ src/components/LoginButtons.jsx
@@ -3,8 +3,11 @@
 
 export default function LoginButtons() {
     function handleLogin(provider){
+        // 환경 변수에서 베이스 URL을 가져옵니다.
+        const baseUrl = process.env.REACT_APP_API_BASE_URL;
+
         // 백엔드 서버의 OAuth2 엔드포인트로 이동
-        window.location.href = `http://localhost:8080/oauth2/authorization/${provider}`;
+        window.location.href = `${baseUrl}/oauth2/authorization/${provider}`;
     }
     return (
         <div className="actions">
