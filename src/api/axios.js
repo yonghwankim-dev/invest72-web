5513313ef27fd11e5e97edf436e3412e32fd666f
yonghwankim-dev <dragonbead95@naver.com>
2026-03-05 15:03:23 +0900

    add console log

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 
+console.log("API Base URL:", process.env.REACT_APP_API_BASE_URL); // 환경 변수 출력 확인
 const HOST = process.env.REACT_APP_API_BASE_URL || "http://localhost:8080";
 const api = axios.create({
         baseURL: HOST,
