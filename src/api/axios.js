4bc07d9eda224639d2c6dd39170470f2b5322e1b
yonghwankim-dev <dragonbead95@naver.com>
2026-03-20 13:27:01 +0900

    fix - change products url

--- src/api/axios.js
+++ src/api/axios.js
@@ -4,8 +4,8 @@ const HOST = process.env.REACT_APP_API_BASE_URL || "http://localhost:8080";
 const api = axios.create({
         baseURL: HOST,
         withCredentials: true, // 모든 세션 요청에 쿠키 포함 
-        xsrfCookieName: "XSRF-TOKEN", // 서버에서 발급하는 CSRF 토큰 쿠키 이름
-        xsrfHeaderName: "X-XSRF-TOKEN", // 요청 헤더에 CSRF 토큰을 첨부할 때 사용할 헤더 이름
-        withXSRFToken: true // CSRF 토큰 자동 첨부 활성화
+        xsrfCookieName: "XSRF-TOKEN", // CSRF 토큰 쿠키 이름
+        xsrfHeaderName: "X-XSRF-TOKEN", // CSRF 토큰 헤더 이름
+        withXSRFToken: true, // CSRF 토큰 자동 포함
     });
 export default api;
