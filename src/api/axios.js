8905c813189465232c3fb62ae30324c3a7aeef89
yonghwankim-dev <dragonbead95@naver.com>
2026-03-29 11:29:02 +0900

    fix - use withXSRFTOken

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,26 +1,11 @@
 import axios from "axios";
-import Cookies from "js-cookie";
 
 const HOST = process.env.REACT_APP_API_BASE_URL || "http://localhost:8080";
 const api = axios.create({
         baseURL: HOST,
-        withCredentials: true // 모든 세션 요청에 쿠키 포함
-    });
-api.interceptors.request.use(
-    (config) => {
-        // 브라우저 쿠키 저장소에서 직접 XSRF-TOKEN을 읽어옵니다.
-        const csrfToken = Cookies.get("XSRF-TOKEN");
-        
-        if (csrfToken) {
-            // 헤더에 수동으로 주입
-            config.headers["X-XSRF-TOKEN"] = csrfToken;
-        }
-        
-        return config;
-    },
-    (error) => {
-        return Promise.reject(error);
-    }
-);
-
+        withCredentials: true, // 모든 세션 요청에 쿠키 포함
+        xsrfCookieName: "XSRF-TOKEN", // CSRF 토큰 쿠키 이름
+        xsrfHeaderName: "X-XSRF-TOKEN", // CSRF 토큰 헤더 이름
+        withXSRFToken: true, // CSRF 토큰 자동 포함
+});
 export default api;
