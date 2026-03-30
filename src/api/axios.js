44e84a7e02f46fc1548227635bfdd9f3ba0ee53c
yonghwankim-dev <dragonbead95@naver.com>
2026-03-30 14:49:36 +0900

    fix - delete console log

--- src/api/axios.js
+++ src/api/axios.js
@@ -11,7 +11,6 @@ const api = axios.create({
 api.interceptors.request.use(config => {
     // 1. 쿠키에서 직접 XSRF-TOKEN 읽기
     const csrfToken = Cookies.get("XSRF-TOKEN");
-    console.log("요청 인터셉터 - CSRF 토큰:", csrfToken);
 
     // 2. 토큰이 존재하고, 요청 메서드가 보호 대상(POST, PUT, DELETE 등)일 때 헤더 추가
     if (csrfToken) {
