32f0a73f05523a14602020de28ab0adcc47fc61c
yonghwankim-dev <dragonbead95@naver.com>
2026-05-07 13:35:18 +0900

    feat - create DashboardProduct.css

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import './DashboardProduct.css'
 import Login from './pages/Login';
 import NavBar from './components/NavBar';
 import useAuth from './hooks/useAuth';
--- src/components/financial_product/DashboardProduct.jsx
+++ src/components/financial_product/DashboardProduct.jsx
@@ -5,13 +5,14 @@ export default function DashboardProduct({statistics}){
     }
     return (
         <>
-            <div>
-                <p>
-                    <label>총자산</label>
+            <div className="card">
+                <h1 className="pageTitle">금융 상품 통계</h1>
+                <p className="statistics_row">
+                    <label>총자산:</label>
                     <span>{statistics.totalBalance.amount} {statistics.totalBalance.currency.name}</span>
                 </p>
-                <p>
-                    <label>예상이자</label>
+                <p className="statistics_row">
+                    <label>예상 이자:</label>
                     <span>{statistics.totalEstimatedInterest.amount} {statistics.totalEstimatedInterest.currency.name}</span>
                 </p>
             </div>
