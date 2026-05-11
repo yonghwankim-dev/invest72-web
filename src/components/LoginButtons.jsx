e2ce70982c9e605f2602ef262415a7bdb03d70cb
YongHwan Kim <dragonbead95@naver.com>
2026-05-11 14:47:37 +0900

    [feat] 로그인 실패시 기능 구현 (#9)
    * feat - add errorMessageBox
    
    * feat - add redirectUri
    
    * feat - delete redirect_uri
    
    * feat - add redirectUri parameter
    
    * fix - delete console error code
    
    * feat - add redirectUri optional value

--- src/components/LoginButtons.jsx
+++ src/components/LoginButtons.jsx
@@ -5,9 +5,11 @@ export default function LoginButtons() {
     function handleLogin(provider){
         // 환경 변수에서 베이스 URL을 가져옵니다.
         const baseUrl = process.env.REACT_APP_API_BASE_URL;
-
+        const redirectUri = process.env.REACT_APP_BASE_URL || window.location.origin;
+    
         // 백엔드 서버의 OAuth2 엔드포인트로 이동
-        window.location.href = `${baseUrl}/oauth2/authorization/${provider}`;
+        window.location.href = `${baseUrl}/oauth2/authorization/${provider}?redirectUri=${encodeURIComponent(redirectUri)}`;
+        
     }
     return (
         <div className="actions">
