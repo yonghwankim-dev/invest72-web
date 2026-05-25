ef7e087d537bae9477f062fae196e5e96274b167
yonghwankim-dev <dragonbead95@naver.com>
2026-05-25 16:25:37 +0900

    refactor - extract component Spinner

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import useAuth from './hooks/useAuth';
 import Home from './pages/Home';
 import FinancialProduct from './pages/FianancialProduct';
 import { useEffect } from 'react';
+import Spinner from './components/Spinner';
 function App() {
   return (
     <BrowserRouter>
@@ -29,34 +30,18 @@ function AppContent(){
         window.history.replaceState({}, document.title, window.location.pathname);
       }
 
-      // 사용자 프로필 정보가 캐싱되어 있다면 API를 호출하지 않음
-      if(localStorage.getItem("user_profile")){
-        setIsAuthLoading(false);
-        return;
-      }
-
       // 로컬 스트로지에 로그인 플래그가 있거나 로그인 리다이렉트한 유저만 사용자 프로필 정보를 조회함
-      if(isLoginSuccess || localStorage.getItem("isLoggedIn") === "true"){
-        try{
-          await getUser();
-        }catch(error){
-          console.error("사용자 프로필 정보 조회 실패", error);
-          localStorage.removeItem("isLoggedIn");
-        }finally{
-          // 성공하든 실패하든, 백엔드 서버 응답이 완료되면 로딩을 해제
-          setIsAuthLoading(false);
-        }
-      }else{
-        // 비로그인 사용자는 기다릴 유저 데이터가 없기 때문에 즉시 렌더링
-        setIsAuthLoading(false);
+      if(localStorage.getItem("isLoggedIn") === "true" && !localStorage.getItem("user_profile")){
+        await getUser();
       }
+      setIsAuthLoading(false);
     };
     initializeAuth();
   }, [getUser, setIsLoggedIn, setIsAuthLoading]);
 
   // 인증정보가 확인되기 전에는 아무런 UI도 노출하지 않고 전역 로딩만 보여줌
   if(isAuthLoading){
-    return renderSpinner();
+    return <Spinner/>
   }
 
   return (
@@ -74,15 +59,6 @@ function AppContent(){
 function parseLoginParam(){
   const searchParams = new URLSearchParams(window.location.search);
   return searchParams.get("login");
-}
-
-function renderSpinner(){
-  return (
-      <div className="globalSplashScreen">
-        <div className="spinner"></div>
-        <p>Invest72 서비스를 준비 중입니다...</p>
-      </div>
-  );  
 }
 
 export default App;
---
+++ src/components/Spinner.jsx
@@ -0,0 +1,11 @@
+
+function Spinner(){
+    return (
+      <div className="globalSplashScreen">
+        <div className="spinner"></div>
+        <p>Invest72 서비스를 준비 중입니다...</p>
+      </div>
+    );
+}
+
+export default Spinner;
