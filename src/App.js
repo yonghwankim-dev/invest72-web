2417fa9146af09702bae67f448fccc116405d959
yonghwankim-dev <dragonbead95@naver.com>
2026-05-25 16:48:52 +0900

    refactor - extract function initializeAuth

--- src/App.js
+++ src/App.js
@@ -20,19 +20,7 @@ function AppContent(){
 
   useEffect(()=>{
     // 로그인 성공후 인증 성공 작업 처리
-    const initializeAuth = async ()=>{
-      if(checkLoginSuccess()){
-        localStorage.setItem("isLoggedIn", "true");
-        // 주소창에서 login 쿼리 파라미터를 제거
-        window.history.replaceState({}, document.title, window.location.pathname);
-      }
-
-      // 로컬 스트로지에 로그인 플래그가 있거나 로그인 리다이렉트한 유저만 사용자 프로필 정보를 조회함
-      if(hasLoggedInFlag() && !hasUserProfileData()){
-        await getUser();
-      }
-    };
-    initializeAuth();
+    initializeAuth(getUser);
   }, [getUser]);
 
   // 인증정보가 확인되기 전에는 아무런 UI도 노출하지 않고 전역 로딩만 보여줌
@@ -50,6 +38,19 @@ function AppContent(){
       </Routes>
     </>
   );
+}
+
+async function initializeAuth(getUser){
+  if(checkLoginSuccess()){
+    localStorage.setItem("isLoggedIn", "true");
+    // 주소창에서 login 쿼리 파라미터를 제거
+    window.history.replaceState({}, document.title, window.location.pathname);
+  }
+
+  // 로컬 스트로지에 로그인 플래그가 있거나 로그인 리다이렉트한 유저만 사용자 프로필 정보를 조회함
+  if(hasLoggedInFlag() && !hasUserProfileData()){
+    await getUser();
+  }
 }
 
 function checkLoginSuccess(){
